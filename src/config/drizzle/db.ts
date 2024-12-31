dd4e37f7ab6bc279e71c51e15f062da4bd11c9a5
woutjedunk <woutjedunk@outlook.be>
2024-12-31 17:10:28 +0100

    created dev DB

--- src/config/drizzle/db.ts
+++ src/config/drizzle/db.ts
@@ -1,4 +1,17 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import "jsr:@std/dotenv/load";
+
 const db = drizzle(Deno.env.get('DATABASE_URL')!);
  
-const result = await db.execute('select 1');
+async function logCurrentTime() {
+    try {
+      const result = await db.execute('SELECT NOW()');
+      console.log('Current time from the database:', result.rows[0].now);
+    } catch (error) {
+      console.error('Error fetching current time:', error);
+    }
+  }
+  
+await logCurrentTime();
+
+export default db;

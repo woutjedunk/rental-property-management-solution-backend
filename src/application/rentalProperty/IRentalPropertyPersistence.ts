c90d7c800b899e1c141cadf1f3e98b40130f9a11
geboerswout <wout.geboers@student.ucll.be>
2025-01-03 21:27:14 +0100

    refactor: rename IRentalPropertyPersistence file and update getByRentalPropertyId parameter type

--- src/application/rentalProperty/IRentalPropertyPersistence.ts
+++ src/application/rentalProperty/IRentalPropertyPersistence.ts
@@ -3,6 +3,6 @@ import { UUID } from "node:crypto";
 
 export interface IRentalPropertyPersistence {
     save(rentalProperty: RentalProperty): Promise<void>;
-    getByRentalPropertyId(id: UUID): Promise<RentalProperty|undefined>;
+    getByRentalPropertyId(id: string): Promise<RentalProperty|undefined>;
     getAll(): Promise<RentalProperty[]>;
 }

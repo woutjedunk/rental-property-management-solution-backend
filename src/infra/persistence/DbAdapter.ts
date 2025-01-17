b91d37239fbe047ced377bb31e7d1ad6298e355e
geboerswout <wout.geboers@student.ucll.be>
2025-01-17 13:00:00 +0100

    starting fresh in application layer (no more Drizzle)

--- src/infra/persistence/DbAdapter.ts
+++
@@ -1,7 +0,0 @@
-
-export interface DbAdapter<DbEntity, DomainEntity> {
-    mapToDb(domainEntity: DomainEntity): DbEntity;
-    maptToDbList(domainEntities: DomainEntity[]): DbEntity[];
-    mapToDomain(dbEntity: DbEntity): DomainEntity;
-    mapToDomainList(dbEntities: DbEntity[]): DomainEntity[];
-}

fb52cdb367b41fb88d4d641328b105c412c98b32
geboerswout <wout.geboers@student.ucll.be>
2025-01-19 00:25:50 +0100

    refactor: update RentalProperty model to include created and updated timestamps

--- src/domain/model/rentalProperty.ts
+++ src/domain/model/rentalProperty.ts
@@ -1,31 +1,28 @@
 import { Address } from "@model/address.ts";
 import { UUID } from "node:crypto";
-import { z } from  "npm:zod";
-
-
 
 export class RentalProperty {
     readonly id: UUID;
-    readonly address?: Address;
-    readonly rentalOwner: string; // later user aan vasthangen
+    address?: Address;
+    rentalOwner: string; // later user aan vasthangen
 
-    readonly madeAt: Date;
-    readonly madeBy: string; // later user aan vasthangen
-    readonly editedAt: Date;
-    readonly editedBy: string;  // later user aan vasthangen
-    readonly rentalName: string;
-    readonly singleBeds: number;
-    readonly doubleBeds: number;
-    readonly storage: string;
+    createdAt: Date;
+    createdBy: string; // later user aan vasthangen
+    updatedAt: Date;
+    updatedBy: string;  // later user aan vasthangen
+    rentalName: string;
+    singleBeds: number;
+    doubleBeds: number;
+    storage: string;
 
     private constructor(
         id: UUID,
         address: Address | undefined,
         rentalOwner: string,
-        madeAt: Date,
-        madeBy: string,
-        editedAt: Date,
-        editedBy: string,
+        createdAt: Date,
+        createdBy: string,
+        updatedAt: Date,
+        updatedBy: string,
         rentalName: string,
         singleBeds: number,
         doubleBeds: number,
@@ -34,10 +31,10 @@ export class RentalProperty {
         this.id = id;
         this.address = address;
         this.rentalOwner = rentalOwner;
-        this.madeAt = madeAt;
-        this.madeBy = madeBy;
-        this.editedAt = editedAt;
-        this.editedBy = editedBy;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.updatedAt = updatedAt;
+        this.updatedBy = updatedBy;
         this.rentalName = rentalName;
         this.singleBeds = singleBeds;
         this.doubleBeds = doubleBeds;
@@ -48,36 +45,24 @@ export class RentalProperty {
         id: UUID,
         address: Address | undefined,
         rentalOwner: string,
-        madeAt: Date,
-        madeBy: string,
-        editedAt: Date,
-        editedBy: string,
+        createdAt: Date,
+        createdBy: string,
+        updatedAt: Date,
+        updatedBy: string,
         rentalName: string,
         singleBeds: number,
         doubleBeds: number,
         storage: string
     ): RentalProperty => {
 
-
-        z.string().parse(rentalOwner);
-
-        z.string().parse(madeBy);
-        z.date().parse(madeAt);
-        z.string().parse(editedBy);
-        z.date().parse(editedAt);
-        z.string().parse(rentalName);
-        z.number().int().parse(singleBeds);
-        z.number().int().parse(doubleBeds);
-        z.string().parse(storage);
-
         return new RentalProperty(
             id,
             address,
             rentalOwner,
-            madeAt,
-            madeBy,
-            editedAt,
-            editedBy,
+            createdAt,
+            createdBy,
+            updatedAt,
+            updatedBy,
             rentalName,
             singleBeds,
             doubleBeds,

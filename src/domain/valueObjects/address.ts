d9e0cd84d79fd4b6999276a222a13bcc758b32be
geboerswout <wout.geboers@student.ucll.be>
2025-01-26 15:11:52 +0100

    changed postalCode to zipcode

--- src/domain/valueObjects/address.ts
+++ src/domain/valueObjects/address.ts
@@ -1,15 +1,15 @@
 export class Address {
     readonly country: string;
     readonly city: string;
-    readonly postalCode: string;
+    readonly zipcode: string;
     readonly street: string;
     readonly houseNumber: string;
     readonly busNumber?: string; 
 
-    constructor(country: string, city: string, postalCode: string, street: string, houseNumber: string, busNumber?: string) {
+    constructor(country: string, city: string, zipcode: string, street: string, houseNumber: string, busNumber?: string) {
         this.country = country;
         this.city = city;
-        this.postalCode = postalCode;
+        this.zipcode = zipcode;
         this.street = street;
         this.houseNumber = houseNumber;
         this.busNumber = busNumber;

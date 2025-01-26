c0fff6c531c47a2aad8662b9dfadf4548fa72b84
geboerswout <wout.geboers@student.ucll.be>
2025-01-26 15:12:13 +0100

    added changeAddress domain method to RentalProperty

--- src/domain/rentalProperty.ts
+++ src/domain/rentalProperty.ts
@@ -12,7 +12,7 @@ export class RentalProperty {
     doubleBeds: number;
     storage: string;
 
-    constructor(id: UUID, address: Address, rentalOwner: string, rentalName: string, singleBeds: number, doublebeds: number, storage: string) {
+    private constructor( id: UUID, address: Address, rentalOwner: string, rentalName: string, singleBeds: number, doublebeds: number, storage: string ) {
         this.id = id;
         this.address = address,
         this.rentalOwner = rentalOwner;
@@ -20,5 +20,9 @@ export class RentalProperty {
         this.singleBeds = singleBeds;
         this.doubleBeds = doublebeds;
         this.storage = storage
+    }
+
+    public changeAddress = ( country: string, city: string, zipCode: string, street: string, houseNumber: string, busNumber?: string ): void => {
+        this.address = new Address(country, city, zipCode, street, houseNumber, busNumber)
     }
 }
